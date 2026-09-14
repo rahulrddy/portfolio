2942020f9e47a93aba57d5e0a1e9b462d2d03bb3
Rahul Reddy Origanti <rahul.origanti@gmail.com>
2026-09-14 18:54:21 -0500

    feat: connect expertise scroll to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { AmbientBackground } from '@/components/home/ambient-background';
+import { ExpertiseScroll } from '@/components/home/expertise-scroll';
 import { Hero } from '@/components/home/hero';
 import { SiteHeader } from '@/components/layout/site-header';
 
 export default function HomePage() {
   return (
-    <main className="relative min-h-svh overflow-hidden bg-[#090c10] text-white">
+    <main className="relative min-h-svh overflow-x-hidden bg-[#090c10] text-white">
       <AmbientBackground />
       <SiteHeader />
       <Hero />
+      <ExpertiseScroll />
       <div id="work" className="relative z-10 h-px" aria-hidden="true" />
     </main>
   );
